867db982c2434e89d9f90eade272fce86bcf029c
Anannya Sharma <Anannyasharma8300@gmail.com>
2022-04-11 13:14:46 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,44 @@
-
-import './App.css';
-import {useState} from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
   const [score, setScore] = useState(76);
-  const [wicket, setwicket] = useState(2);
-  const [over, setover] = useState(0);
-  const [ball, setball] = useState(50);
+  const [wicket, setWicket] = useState(2);
+  const [over, setOver] = useState(8);
+  const [ball, setBall] = useState(50);
 
-  const myScore = (value) =>{
-    setScore(score + value);
+  const handleScore = (value) => {
+    setScore(score <= 100 ? score + value : score);
   };
 
-  const myWicket = (value) =>{
-    setwicket(wicket + value);
+  const handleWicket = (value) => {
+    setWicket(score <= 100 ? wicket + value : wicket);
   };
 
-  const myBall = (value) =>{
-    setball(ball + value);
-    setover(over + Math.floor(ball/6));
+  const handleBall = (value) => {
+    if(score <= 100) {
+      setBall(ball + value);
+      setOver(ball % 6 === 0 ? over + value : Math.floor(ball / 6));
+    } 
   };
 
-  
   return (
-    <div className="App">
+    <div className='App'>
       <h3>India:</h3>
-      <div className="banner">
+      <div className='banner'>
         <div>
-          Score:{" "}
-          <h1 className="scoreCount">
-            {
-              score
-            }
-          </h1>
+          Score: <h1 className='scoreCount'>{score}</h1>
         </div>
         <div>
-          Wicket:{" "}
-          <h1 className="wicketCount">
-            {
-              wicket
-            }
-          </h1>
+          Wicket: <h1 className='wicketCount'>{wicket <= 12 ? wicket : 12}</h1>
         </div>
 
         <div>
           Over:{" "}
-          <h1 className="overCount">
+          <h1 className='overCount'>
             {
+              over + "." + (ball % 6)
               // Show Over here in the format: "over.ball" eg: 4.5 means 4th over and 5th ball
               // if 1 more ball is thrown then over is now 5.0
               // you have to write logic to form this string from current ball number.
@@ -56,52 +47,60 @@ function App() {
         </div>
       </div>
 
-      <div className="addScore">
+      <div className='addScore'>
         Add Score
         {/* these buttons should add the respective amount in the score */}
-        <button 
-        onClick={()=>{
-        myScore(1);
-        }}
-        className="addScore1">
-        Add 1</button>
-
-        <button 
-        onClick={()=>{
-        myScore(4);
-        }}
-        className="addScore4">Add 4</button>
-
-        <button 
-        onClick={()=>{
-        myScore(6);
-        }}
-        className="addScore6">Add 6</button>
+        <button
+          onClick={() => {
+            handleScore(1);
+          }}
+          className='addScore1'
+        >
+          Add 1
+        </button>
+        <button
+          onClick={() => {
+            handleScore(4);
+          }}
+          className='addScore4'
+        >
+          Add 4
+        </button>
+        <button
+          onClick={() => {
+            handleScore(6);
+          }}
+          className='addScore6'
+        >
+          Add 6
+        </button>
       </div>
 
-
-
-      <div className="addWicket">
+      <div className='addWicket'>
         Add Wicket
         {/* Increase the count of wicket */}
-        <button 
-        onClick={()=>{
-          myWicket(1);
-        }}
-        className='add1'>Add 1 wicket</button>
+        <button
+          onClick={() => {
+            handleWicket(1);
+          }}
+        >
+          Add 1 wicket
+        </button>
       </div>
 
-      <div className="addBall">
+      <div className='addBall'>
         Add ball
         {/* Increase the total number of balls thrown here. */}
-        <button 
-        onClick={()=>{
-        myBall(1);
-        }}
-        className='addoneBall'>Add 1</button>
+        <button
+          onClick={() => {
+            handleBall(1);
+          }}
+        >
+          Add 1
+        </button>
       </div>
 
-      {/* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
+      {<h1>{score > 100 ? "India Won" : ""} </h1>}
     </div>
   );
 }
